d2f2636eb025095dd5f4a900cd8a0b9da31e82ac
sulshynkitaTeam <102748955+sulshynkitaTeam@users.noreply.github.com>
2022-04-29 06:52:37 +0600

    Delete Lab6 directory

--- Lab6/src/app/db.ts
+++
@@ -1,9 +0,0 @@
-import {Post} from "./models";
-
-export const POSTS: Post[] = [
-  {id: 1, title: "title 1", body: "There is the title 1"},
-  {id: 2, title: "title 2", body: "There is the title 2"},
-  {id: 3, title: "title 3", body: "There is the title 3"},
-  {id: 4, title: "title 4", body: "There is the title 4"},
-]
-
